980223b5435e9d8fd32cb513683a82ed5ec256f3
ramtej473 <22bk1a6622@stpetershyd.com>
2024-12-24 14:32:04 +0530

    first commit

---
+++ Demo2.java
@@ -0,0 +1,23 @@
+package ramtej;
+
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		
+		String name = "ramu";
+		// RAMU
+		
+		char ch = name.charAt(0);
+		int diff = (int)('d'-'D');
+		
+		diff = diff<0?-1 * diff:diff;
+		
+		if(ch >='a' && ch <='z') {
+			ch = (char)(ch -diff);
+		}
+		System.out.println(ch);
+		
+	}
+
+}
---
+++ bankdemo.java
@@ -0,0 +1,23 @@
+package ramtej;
+
+public class bankdemo {
+
+	public static void main(String[] args) {
+	
+		BankAccount bankAcct = new BankAccount(1000, "Raju", 5000,
+				"hyd",
+				"9845098450") ;
+		
+		System.out.println(bankAcct);
+		
+		bankAcct.deposit(5000);
+		bankAcct.checkBalance();
+		
+		
+		bankAcct.withDraw(4000);
+		bankAcct.checkBalance();
+		
+		
+	}
+
+}
